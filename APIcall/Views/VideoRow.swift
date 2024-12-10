2a1e4fb5b15094a00df2d89063e2e9c2d0df35ef
Rihan <100439488+DsaRihan@users.noreply.github.com>
2024-12-10 09:23:54 +0530

    updated videoRow

--- APIcall/Views/VideoRow.swift
+++ APIcall/Views/VideoRow.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct VideoRow: View {
+    
+    var video:Video
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading ){
+            if let url = URL(string: video.snippet?.thumbnails?.medium?.url ?? ""){
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                } placeholder: {
+                    ProgressView()
+                }
+                
+            }
+            Text(video.snippet?.title ?? "")
+                .bold()
         }
         .padding()
     }
 }
 
-#Preview {
-    VideoRow()
-}
+
