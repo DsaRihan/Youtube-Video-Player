5cc34924abbb78731e5522119c38bff72f68ffab
Rihan <100439488+DsaRihan@users.noreply.github.com>
2024-12-10 09:25:44 +0530

    vodeoDetail

--- APIcall/Views/VideoDetail.swift
+++ APIcall/Views/VideoDetail.swift
@@ -6,19 +6,37 @@
 //
 
 import SwiftUI
+import YouTubePlayerKit
+import YouTubePlayerKit_YouTubePlayerKit
 
 struct VideoDetail: View {
+    var video:Video
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (alignment: .leading){
+            
+            let youTubePlayer = YouTubePlayer(
+                source: .video(id: video.snippet?.resourceId?.videoId),
+                configuration: .init(
+                    autoPlay: true
+                )
+            )
+            YouTubePlayerView(
+                youTubePlayer
+            )
+            
+            ScrollView{
+                VStack(alignment: .leading, spacing: 30){
+                    Text(video.snippet?.title ?? "")
+                        .font(.headline)
+                        .bold()
+                    
+                    Text(video.snippet?.description ?? "")
+                }
+            }
         }
         .padding()
+        .scrollIndicators(.hidden)
     }
 }
 
-#Preview {
-    VideoDetail()
-}
+
